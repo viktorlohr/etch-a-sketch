c5e4c9783ca716fe298e2df88764afdfefbe1235
Viktor Lohr <viktor.lohr01@gmail.com>
2026-09-19 18:53:32 +0200

    Implement basic functionality

--- app.js
+++ app.js
@@ -1,32 +1,50 @@
 const NOF_SQUARES = 16;
 const TOTAL_PIXELS = 512;
 
-// createSquare(NOF_SQUARES);
+createSquares(NOF_SQUARES);
+
+const nofSquaresInputEl = document.getElementById('number-of-squares');
+// nofSquaresInputEL.placeholder = NOF_SQUARES;
+
+const nofSquaresBtn = document.querySelector('form button');
+nofSquaresBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  createSquares(Number(nofSquaresInputEl.value))
+})
 
 function createSquares(nofSquares) {
+  const squareContainerEl = document.querySelector('container');
+  console.log('foo')
+  
+  if (!((typeof nofSquares) === "number") || nofSquares === 0) {
+    nofSquares = NOF_SQUARES;
+  }
+  else if (nofSquares < 2) {
+    nofSquares = 2;
+  } else if (nofSquares > 100) {
+    nofSquares = 100;
+  }
+  
   let squareLength = TOTAL_PIXELS / nofSquares;
 
   let squares = [];
   let lineEls = [];
 
-  let containerEl = document.querySelector('container');
+  squareContainerEl.replaceChildren();
 
-  containerEl.replaceChildren();
-
-  for (let i = 0; i < NOF_SQUARES; i++) {
+  for (let i = 0; i < nofSquares; i++) {
 
     squares[i] = [];
     
     lineEls[i] = document.createElement('div');
     lineEls[i].style.display = 'flex';
 
-    for (let j = 0; j < NOF_SQUARES; j++) {
+    for (let j = 0; j < nofSquares; j++) {
       squares[i][j] = document.createElement('div');
 
       squares[i][j].style.backgroundColor = 'lightgray';
       squares[i][j].style.width = squareLength + 'px';
       squares[i][j].style.height = squareLength + 'px';
-      squares[i][j].style.border = '1px solid red';
 
       squares[i][j].addEventListener('mouseenter', () => {
         squares[i][j].style.backgroundColor = 'black';
@@ -36,7 +54,7 @@ function createSquares(nofSquares) {
       lineEls[i].appendChild(squares[i][j]);
     }
 
-    containerEl.appendChild(lineEls[i]);
+    squareContainerEl.appendChild(lineEls[i]);
   }
 }
 
